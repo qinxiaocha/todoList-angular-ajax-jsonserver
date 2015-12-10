4cde3ee9635701befa5f36675e7c1b2b4072cc79
Fenglei_Qin@epam.com <Fenglei_Qin@epam.com>
2015-12-10 20:19:13 +0800

    add

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-angular.module('todomvc', ["ngRoute","ngResource"]).
-config(['$routeProvider', function($routeProvider) {
+angular.module('todomvc', ["ngRoute","ngResource"])
+.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
     controller: "TodoCtrl",
     templateUrl: 'todomvc-index.html'
   })
   .otherwise({redirectTo: '/'});
-}]);
+}])
+.controller('myTodo',['$scope','$http',function($scope,$http){
+	$http.get("../routes.json").then(function(data){
+		$scope.data = data;
+	});
+}];;
